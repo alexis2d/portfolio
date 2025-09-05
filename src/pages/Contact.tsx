91849f5c0315e1ef7e62f2fdf94fbb3a325207e9
Alexis Duquenet <alexisduquenet@gmail.com>
2025-09-05 14:38:47 +0200

    Minor changes

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -6,7 +6,7 @@ export default function Contact() {
       <h2 className="text-3xl font-bold text-indigo-400">Contact</h2>
       <p className="mt-4">
         Si mon profil t'intéresse, tu peux me contacter par <Link to="mailto:alexis2d.pro@gmail.com">email</Link> ou <Link to="https://linkedin.com/in/alexis-duquenet-78bbb5202" target="_blank">Linkedin</Link>, ce sera avec plaisir 😎<br></br>
-        Et tu peux également checker mon <Link to="/cv.pdf">CV</Link> ainsi que mon profil <Link to="https://github.com/alexis2d" target="_blank">Github</Link>
+        Et tu peux également checker mon profil <Link to="https://github.com/alexis2d" target="_blank">Github</Link>
       </p>
     </section>
   );

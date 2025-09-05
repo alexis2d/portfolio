3710f663ffdd9a481f19bf11c5c0bea2b132ce16
Alexis Duquenet <alexisduquenet@gmail.com>
2025-09-05 14:00:48 +0200

    Style upgrade + better utilisation of react (consts, loops, etc)

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,85 +4,81 @@ import { useState } from "react";
 export default function Projects() {
   const [activeDiv, setActiveDiv] = useState("work");
 
+  const projetcTypes = [
+    { label: "Projets professionnels", value: "work" },
+    { label: "Projets personnels", value: "self" },
+    { label: "Projets scolaires", value: "school" }
+  ];
+
+  const projects = [
+    {
+      type: "work", 
+      date: "2021 - Aujourd'hui",
+      title: "Emile's", 
+      description: "C'est littérallement le projet de ma (courte) carrière !<br/><br/>Présent depuis environ 4 ans (d'abord en alternance puis en CDI), j'ai évolué en même temps que l'entreprise et le projet, ce qui m'a permis d'être là où j'en suis 👌", 
+      technos : ["Laravel", "PrestaShop", "AWS", "MySQL"],
+      link: "https://www.emiles.fr"
+    },
+    {
+      type: "self", 
+      date: "2025",
+      title: "Portfolio", 
+      description: "Bah oui, on est dessus, mais bon...<br/><br/>En tout cas, j\'ai réalisé ce portfolio avant tout pour m\'introduire à React, mais également parce que c'est un projet que j'ai en tête depuis un certains temps.", 
+      technos : ["React", "TailwindCSS"],
+      link: "https://github.com/alexis2d/portfolio"
+    },
+    {
+      type: "self", 
+      date: "2025",
+      title: "KHWatchFace", 
+      description: "Quand on s'achète un nouveau jouet, nous les développeurs on veut absolument avoir NOTRE version...<br/><br/>Du coup, j'ai créé ma propre watchface à l'image de ma licence de jeu vidéo préférée dès que j'ai acheté ma smartwatch.", 
+      technos : ["Android", "WatchFaceFormat", "XML"],
+      link: "https://github.com/alexis2d/KHWatchFace"
+    },
+    {
+      type: "school", 
+      date: "2023",
+      title: "Appli Météo", 
+      description: "Mon premier projet d'appli native !<br/><br/>Comme son nom l'indique, il s'agit d'une simple application mobile permettant de voir la météo, ni plus ni moins.", 
+      technos : ["Android", "Java"],
+      link: "https://github.com/alexis2d/cefim_android_weather_app"
+    }
+  ];
+
+  const filteredProjects = projects.filter(
+    (project) => project.type === activeDiv
+  );
+
   return (
     <section className="max-w-5xl mx-auto mt-10">
       <h2 className="text-3xl font-bold text-indigo-400 mb-6">Mes Projets</h2>
       <div className="flex gap-2 mb-4 justify-center">
-        <button 
-          onClick={() => setActiveDiv("work")} 
-          className="px-3 py-2 bg-gray-100 dark:bg-gray-900 rounded"
+        {projetcTypes.map((type) => (
+          <button 
+          key={type.value}
+          onClick={() => setActiveDiv(type.value)} 
+          className={`px-3 py-2 bg-gray-100 dark:bg-gray-900 rounded ${activeDiv === type.value ? 'selected' : ''}`}
         >
-          Projets professionnels
+          {type.label}
         </button>
-        <button 
-          onClick={() => setActiveDiv("self")} 
-          className="px-3 py-2 bg-gray-100 dark:bg-gray-900 rounded"
-        >
-          Projets personnels
-        </button>
-        <button 
-          onClick={() => setActiveDiv("school")} 
-          className="px-3 py-2 bg-gray-100 dark:bg-gray-900 rounded"
-        >
-          Projets scolaires
-        </button>
+        ))}
       </div>
-      {activeDiv === "work" && (
-        <div>
-        <Link to="https://www.emiles.fr" target="_blank">
-          <div className="bg-gray-100 dark:bg-gray-900 rounded-xl p-4 shadow-md link">
-            <h3 className="text-xl font-bold">Emile's</h3>
-            <p>
-              C'est littérallement le projet de ma (courte) carrière !<br></br><br></br>
-              Présent depuis environ 4 ans (d'abord en alternance puis en CDI), j'ai évolué en même temps que l'entreprise et le projet, ce qui m'a permis d'être là où j'en suis 👌<br></br><br></br>
-              Projet multiple<br></br>
-              <span className="text-left">
-                ➡️ API en Laravel<br></br>
-                ➡️ Site e-commerce en PrestaShop<br></br> 
-              </span>
-            </p>
-          </div>
-        </Link>
+      <div className={filteredProjects.length > 1 ? "grid sm:grid-cols-2 md:grid-cols-3 gap-6" : ""}>
+        {filteredProjects.map((project) => (
+          <Link key={project.title} to={project.link} target="_blank">
+            <div className="bg-gray-100 dark:bg-gray-900 rounded-xl p-4 shadow-md relative link">
+              <p className="bg-indigo-400 dark:bg-indigo-100 text-gray-100 dark:text-indigo-800 text-sm top-0 right-0 p-3 rounded-bl-lg rounded-tr-lg absolute">{project.date}</p>
+              <h3 className="text-xl font-bold">{project.title}</h3>
+              <br/><hr/><br/>
+              <p dangerouslySetInnerHTML={{ __html: project.description }}></p>
+              <br/><hr/><br/>
+              {project.technos.map((techno, index) => (
+                <span key={project.title + " - techno " + index} className="inline-block bg-indigo-400 dark:bg-indigo-100 text-gray-100 dark:text-indigo-800 text-xs px-2 py-1 rounded-full mr-2 mb-2">{techno}</span>
+              ))}
+            </div>
+          </Link>
+        ))}
       </div>
-      )}
-      {activeDiv === "self" && (
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-6">
-        <Link to="https://github.com/alexis2d/portfolio" target="_blank">
-          <div className="bg-gray-600 hover:bg-gray-500 dark:bg-gray-200 dark:hover:bg-gray-100 rounded-xl p-4 shadow-md">
-            <h3 className="text-xl font-bold text-gray-200 dark:text-gray-800">Portfolio</h3>
-            <p className="text-gray-100 dark:text-gray-700">
-              Bah oui, on est dessus, mais bon...<br></br><br></br>
-              En tout cas, j'ai réalisé ce portfolio avant tout pour m'introduire à React, mais également pour avoir un endroit où je peux partager mes créations.<br></br><br></br>
-              Réalisé avec React, TypeScript et TailwindCSS.
-            </p>
-          </div>
-        </Link>
-        <Link to="https://github.com/alexis2d/KHWatchFace" target="_blank">
-          <div className="bg-gray-600 hover:bg-gray-500 dark:bg-gray-200 dark:hover:bg-gray-100 rounded-xl p-4 shadow-md">
-            <h3 className="text-xl font-bold text-gray-200 dark:text-gray-800">KHWatchFace</h3>
-            <p className="text-gray-100 dark:text-gray-700">
-              Quand on s'achète un nouveau jouet, nous les développeurs on veut absolument avoir NOTRE version...<br></br><br></br>
-              Du coup, j'ai créé ma propre watchface à l'image de ma licence de jeu vidéo préférée dès que j'ai acheté ma smartwatch.<br></br><br></br>
-              Réalisé avec le récent WatchFaceFormat de Google (en gros, principalement du XML), pour WearOS.
-            </p>
-          </div>
-        </Link>
-      </div>
-      )}
-      {activeDiv === "school" && (
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-6">
-        <Link to="https://github.com/alexis2d/cefim_android_weather_app" target="_blank">
-          <div className="bg-gray-600 hover:bg-gray-500 dark:bg-gray-200 dark:hover:bg-gray-100 rounded-xl p-4 shadow-md">
-            <h3 className="text-xl font-bold text-gray-200 dark:text-gray-800">Appli Météo</h3>
-            <p className="text-gray-100 dark:text-gray-700">
-              Mon premier projet d'appli native !<br></br><br></br>
-              Comme son nom l'indique, il s'agit d'une simple application mobile permettant de voir la météo, ni plus ni moins.<br></br><br></br>
-              Réalisé avec Java sur Android Studio.
-            </p>
-          </div>
-        </Link>
-      </div>
-      )}
     </section>
   );
 }
